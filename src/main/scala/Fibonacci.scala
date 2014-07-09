a2a577f76f1be676ad112e412a25331cd69a1b06
Sebastian Gebski <sebastian@gebski.net>
2014-07-10 00:26:31 +0200

    Let's get started - Exercise #1: Recursive Fibonacci, da func way

--- src/main/scala/Fibonacci.scala
+++ src/main/scala/Fibonacci.scala
@@ -1,3 +1,20 @@
+import scala.annotation.tailrec
+
 object Fibonacci {
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    n match {
+      case 1 => 1
+      case 2 => 1
+      case _ => smartFib(1, 1, 3, n)
+    }
+  }
+
+  @tailrec
+  private def smartFib(fa: Int, fb: Int, k: Int, n: Int): Int = {
+    if (k == n) {
+      fa + fb
+    } else {
+      smartFib(fb, fa + fb, k + 1, n)
+    }
+  }
 }
--- src/test/scala/FibonacciSpec.scala
+++ src/test/scala/FibonacciSpec.scala
@@ -11,5 +11,14 @@ class FibonacciSpec extends FunSpec {
     it("should be 1 for 2") {
       assert(Fibonacci.fib(2) == 1)
     }
+
+    it("should be fib(n-1) + fib(n-2) for n") {
+      for (i <- 3 to 10) {
+        val a = Fibonacci.fib(i)
+        val b = Fibonacci.fib(i - 1)
+        val c = Fibonacci.fib(i - 2)
+        assert(a == b + c, "; n = " + i + "; f(n) = " + a + "; f(n-1) = " + b + "; f(n-2) = " + c)
+      }
+    }
   }
 }
