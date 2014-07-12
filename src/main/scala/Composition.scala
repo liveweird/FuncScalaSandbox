01bdc2c6e49bed0220c1191b577694a1de06e10a
Sebastian Gebski <sebastian@gebski.net>
2014-07-13 00:16:38 +0200

    Currying

--- src/main/scala/Composition.scala
+++ src/main/scala/Composition.scala
@@ -1,7 +1,9 @@
 package net.gebski.FuncScalaSandbox.Chapter2
 
 object Composition {
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) = ???
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = (a: A) => { (b: B) => f(a, b) }
+
   def uncurry[A,B,C](f: A => B => C): (A, B) => C = ???
+
   def compose[A,B,C](f: B => C, g: A => B): A => C = ???
 }
---
+++ src/test/scala/CompositionSpec.scala
@@ -0,0 +1,12 @@
+package net.gebski.FuncScalaSandbox.Chapter2
+
+import org.scalatest.FunSpec
+
+class CompositionSpec extends FunSpec {
+  describe("Currying") {
+    it("works for A + B + C") {
+      val func1 = (a: Int, b: Int) => { a + b }
+      assert(func1(2,3) == Composition.curry(func1)(2)(3))
+    }
+  }
+}
