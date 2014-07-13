9f5a21791737cd475f2825f5394a584c89ee70ac
Sebastian Gebski <sebastian@gebski.net>
2014-07-13 21:45:07 +0200

    Uncurrying

--- src/main/scala/Composition.scala
+++ src/main/scala/Composition.scala
@@ -3,7 +3,7 @@ package net.gebski.FuncScalaSandbox.Chapter2
 object Composition {
   def curry[A,B,C](f: (A, B) => C): A => (B => C) = (a: A) => { (b: B) => f(a, b) }
 
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C = ???
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a: A, b: B) => { f(a)(b) }
 
   def compose[A,B,C](f: B => C, g: A => B): A => C = ???
 }
--- src/test/scala/CompositionSpec.scala
+++ src/test/scala/CompositionSpec.scala
@@ -4,9 +4,16 @@ import org.scalatest.FunSpec
 
 class CompositionSpec extends FunSpec {
   describe("Currying") {
-    it("works for A + B + C") {
+    it("works for A + B = C") {
       val func1 = (a: Int, b: Int) => { a + b }
       assert(func1(2,3) == Composition.curry(func1)(2)(3))
+    }
+  }
+
+  describe("Uncurrying") {
+    it("works for A + B = C") {
+      val func1 = (a: Int) => (b: Int) => { a + b }
+      assert(func1(2)(3) == Composition.uncurry(func1)(2,3))
     }
   }
 }
