a78c03b065b278bf92210401f84d456a2149448f
Sebastian Gebski <sebastian@gebski.net>
2014-07-13 21:50:16 +0200

    Composition

--- src/test/scala/CompositionSpec.scala
+++ src/test/scala/CompositionSpec.scala
@@ -16,4 +16,12 @@ class CompositionSpec extends FunSpec {
       assert(func1(2)(3) == Composition.uncurry(func1)(2,3))
     }
   }
+
+  describe("Compose") {
+    it("works for y = x + 2, z = y * 3") {
+      val func1 = (a: Int) => a + 2
+      val func2 = (a: Int) => a * 3
+      assert(func1(func2(7)) == Composition.compose(func1, func2)(7))
+    }
+  }
 }
